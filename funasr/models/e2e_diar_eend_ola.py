8f540ebf974f23e2f77e3da4baac20e07c532347
speech_asr <wangjiaming.wjm@alibaba-inc.com>
2023-04-02 12:51:05 +0800

    update

--- funasr/models/e2e_diar_eend_ola.py
+++ funasr/models/e2e_diar_eend_ola.py
@@ -14,6 +14,8 @@ from typeguard import check_argument_types
 from funasr.models.frontend.wav_frontend import WavFrontendMel23
 from funasr.modules.eend_ola.encoder import EENDOLATransformerEncoder
 from funasr.modules.eend_ola.encoder_decoder_attractor import EncoderDecoderAttractor
+from funasr.modules.eend_ola.utils.losses import batch_pit_n_speaker_loss, standard_loss, cal_power_loss
+from funasr.modules.eend_ola.utils.power import create_powerlabel
 from funasr.modules.eend_ola.utils.power import generate_mapping_dict
 from funasr.torch_utils.device_funcs import force_gatherable
 from funasr.train.abs_espnet_model import AbsESPnetModel
@@ -27,7 +29,7 @@ else:
         yield
 
 
-def pad_attractor(att, max_n_speakers):
+def pad_tensor(att, max_n_speakers):
     C, D = att.shape
     if C < max_n_speakers:
         att = torch.cat([att, torch.zeros(max_n_speakers - C, D).to(torch.float32).to(att.device)], dim=0)
@@ -59,7 +61,6 @@ class DiarEENDOLAModel(AbsESPnetModel):
         if mapping_dict is None:
             mapping_dict = generate_mapping_dict(max_speaker_num=self.max_n_speaker)
             self.mapping_dict = mapping_dict
-        # PostNet
         self.postnet = nn.LSTM(self.max_n_speaker, n_units, 1, batch_first=True)
         self.output_layer = nn.Linear(n_units, mapping_dict['oov'] + 1)
 
@@ -74,9 +75,9 @@ class DiarEENDOLAModel(AbsESPnetModel):
         return emb
 
     def forward_post_net(self, logits, ilens):
-        maxlen = torch.max(ilens).to(torch.int).item()
+        maxlen = torch.max(ilens)
         logits = nn.utils.rnn.pad_sequence(logits, batch_first=True, padding_value=-1)
-        logits = nn.utils.rnn.pack_padded_sequence(logits, ilens.cpu().to(torch.int64), batch_first=True,
+        logits = nn.utils.rnn.pack_padded_sequence(logits, ilens, batch_first=True,
                                                    enforce_sorted=False)
         outputs, (_, _) = self.postnet(logits)
         outputs = nn.utils.rnn.pad_packed_sequence(outputs, batch_first=True, padding_value=-1, total_length=maxlen)[0]
@@ -86,98 +87,39 @@ class DiarEENDOLAModel(AbsESPnetModel):
 
     def forward(
             self,
-            speech=None,
-            text=None,
+            xs=None,
+            ts=None,
             orders=None,
     ) -> Tuple[torch.Tensor, Dict[str, torch.Tensor], torch.Tensor]:
-        """Frontend + Encoder + Decoder + Calc loss
+        n_speakers = [t.shape[1] for t in ts]
+        ilens = [x.shape[0] for x in xs]
+        emb = self.forward_core(xs, ilens)
+        batch_size = len(xs)
+        attractor_loss, attractors = self.eda([e[order] for e, order in zip(emb, orders)], n_speakers)
 
-        Args:
-            speech: (Batch, Length, ...)
-            speech_lengths: (Batch, )
-            text: (Batch, Length)
-            text_lengths: (Batch,)
-        """
-        assert text_lengths.dim() == 1, text_lengths.shape
-        # Check that batch_size is unified
-        assert (
-                speech.shape[0]
-                == speech_lengths.shape[0]
-                == text.shape[0]
-                == text_lengths.shape[0]
-        ), (speech.shape, speech_lengths.shape, text.shape, text_lengths.shape)
-        batch_size = speech.shape[0]
+        # PIT
+        ys = [torch.matmul(e, att.permute(1, 0)) for e, att in zip(emb, attractors)]
+        max_n_speakers = max(n_speakers)
+        ts_padded = [pad_tensor(t, max_n_speakers) for t in ts]
+        ys_padded = [pad_tensor(y, max_n_speakers) for y in ys]
+        _, labels = batch_pit_n_speaker_loss(ys_padded, ts_padded, n_speakers)
+        pit_loss = standard_loss(ys, labels)
 
-        # for data-parallel
-        text = text[:, : text_lengths.max()]
+        # PSE
+        with torch.no_grad():
+            power_ts = [create_powerlabel(label.cpu().numpy(), self.mapping_dict, self.max_n_speaker).
+                            to(emb[0].device, non_blocking=True) for label in labels]
+        pad_attractors = [pad_tensor(att, self.max_n_speaker) for att in attractors]
+        pse_ys = [torch.matmul(e, pad_att.permute(1, 0)) for e, pad_att in zip(emb, pad_attractors)]
+        pse_logits = self.forward_post_net(pse_ys, torch.tensor(ilens))
+        pse_loss = cal_power_loss(pse_logits, power_ts)
+        loss = pse_loss + pit_loss + attractor_loss * self.attractor_loss_ratio
 
-        # 1. Encoder
-        encoder_out, encoder_out_lens = self.enc(speech, speech_lengths)
-        intermediate_outs = None
-        if isinstance(encoder_out, tuple):
-            intermediate_outs = encoder_out[1]
-            encoder_out = encoder_out[0]
-
-        loss_att, acc_att, cer_att, wer_att = None, None, None, None
-        loss_ctc, cer_ctc = None, None
         stats = dict()
-
-        # 1. CTC branch
-        if self.ctc_weight != 0.0:
-            loss_ctc, cer_ctc = self._calc_ctc_loss(
-                encoder_out, encoder_out_lens, text, text_lengths
-            )
-
-            # Collect CTC branch stats
-            stats["loss_ctc"] = loss_ctc.detach() if loss_ctc is not None else None
-            stats["cer_ctc"] = cer_ctc
-
-        # Intermediate CTC (optional)
-        loss_interctc = 0.0
-        if self.interctc_weight != 0.0 and intermediate_outs is not None:
-            for layer_idx, intermediate_out in intermediate_outs:
-                # we assume intermediate_out has the same length & padding
-                # as those of encoder_out
-                loss_ic, cer_ic = self._calc_ctc_loss(
-                    intermediate_out, encoder_out_lens, text, text_lengths
-                )
-                loss_interctc = loss_interctc + loss_ic
-
-                # Collect Intermedaite CTC stats
-                stats["loss_interctc_layer{}".format(layer_idx)] = (
-                    loss_ic.detach() if loss_ic is not None else None
-                )
-                stats["cer_interctc_layer{}".format(layer_idx)] = cer_ic
-
-            loss_interctc = loss_interctc / len(intermediate_outs)
-
-            # calculate whole encoder loss
-            loss_ctc = (
-                               1 - self.interctc_weight
-                       ) * loss_ctc + self.interctc_weight * loss_interctc
-
-        # 2b. Attention decoder branch
-        if self.ctc_weight != 1.0:
-            loss_att, acc_att, cer_att, wer_att = self._calc_att_loss(
-                encoder_out, encoder_out_lens, text, text_lengths
-            )
-
-        # 3. CTC-Att loss definition
-        if self.ctc_weight == 0.0:
-            loss = loss_att
-        elif self.ctc_weight == 1.0:
-            loss = loss_ctc
-        else:
-            loss = self.ctc_weight * loss_ctc + (1 - self.ctc_weight) * loss_att
-
-        # Collect Attn branch stats
-        stats["loss_att"] = loss_att.detach() if loss_att is not None else None
-        stats["acc"] = acc_att
-        stats["cer"] = cer_att
-        stats["wer"] = wer_att
-
-        # Collect total loss stats
+        stats["pse_loss"] = pse_loss.detach()
+        stats["attractor_loss"] = attractor_loss.detach()
         stats["loss"] = torch.clone(loss.detach())
+        stats["batch_size"] = batch_size
 
         # force_gatherable: to-device and to-tensor if scalar for DataParallel
         loss, stats, weight = force_gatherable((loss, stats, batch_size), loss.device)
@@ -214,10 +156,10 @@ class DiarEENDOLAModel(AbsESPnetModel):
                 NotImplementedError('n_speakers or threshold has to be given.')
         raw_n_speakers = [att.shape[0] for att in attractors_active]
         attractors = [
-            pad_attractor(att, self.max_n_speaker) if att.shape[0] <= self.max_n_speaker else att[:self.max_n_speaker]
+            pad_tensor(att, self.max_n_speaker) if att.shape[0] <= self.max_n_speaker else att[:self.max_n_speaker]
             for att in attractors_active]
         ys = [torch.matmul(e, att.permute(1, 0)) for e, att in zip(emb, attractors)]
-        logits = self.forward_post_net(ys, speech_lengths)
+        logits = self.forward_post_net(ys, speech_lengths.cpu().to(torch.int64))
         ys = [self.recover_y_from_powerlabel(logit, raw_n_speaker) for logit, raw_n_speaker in
               zip(logits, raw_n_speakers)]
 
