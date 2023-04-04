7184896f3d21ddbde4e5c534252e8c39912aad04
lyblsgo <lyblsgo@163.com>
2023-04-04 17:43:41 +0800

    modify tester_rtf

--- funasr/runtime/onnxruntime/tester/tester_rtf.cpp
+++ funasr/runtime/onnxruntime/tester/tester_rtf.cpp
@@ -80,7 +80,7 @@ int main(int argc, char *argv[])
 
         if(Result){
             string msg = RapidAsrGetResult(Result, 0);
-            printf("Result: %s \n", msg);
+            printf("Result: %s \n", msg.c_str());
 
             snippet_time = RapidAsrGetRetSnippetTime(Result);
             total_length += snippet_time;
