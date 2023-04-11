506bb0bffcda5933334fcd1b1668af2b989ee4bd
speech_asr <wangjiaming.wjm@alibaba-inc.com>
2023-04-11 11:17:44 +0800

    update

--- egs/callhome/EEND-OLA/run.sh
+++ egs/callhome/EEND-OLA/run.sh
@@ -15,16 +15,13 @@ infer_cmd=utils/run.pl
 # general configuration
 feats_dir="/nfs/wangjiaming.wjm/EEND_ARK_DATA" #feature output dictionary
 exp_dir="."
-lang=en
 dumpdir=dump/simu_data/data
-feats_type=fbank
-token_type=bpe
+dumpdir_chunk2000=dump/simu_data_chunk2000/data
 dataset_type=diarization
 simu_2spk_scp=feats_2spk.scp
 simu_scp=feats.scp
-type=kaldi_ark
-stage=3
-stop_stage=4
+stage=5
+stop_stage=6
 
 # feature configuration
 feats_dim=345
@@ -256,6 +253,52 @@ if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
     nlast_models=`eval echo ${exp_dir}/exp/${simu_allspk_model_dir}/{$average_start..$average_end}epoch.pb`
     avg_model=${exp_dir}/exp/${simu_allspk_model_dir}/${average_start}-${average_end}epoch.ave.pb
     average_nlast_models.py $avg_model $nlast_models
+fi
+
+simu_allspk_chunk2000_model_dir="baseline_$(basename "${train_simu_allspk_config}" .yaml)_${tag}/finetune_chunk2000"
+if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
+    echo "stage 5: Training on all-spk simulated dataset with chunksize 2000"
+    mkdir -p ${exp_dir}/exp/${simu_allspk_chunk2000_model_dir}
+    mkdir -p ${exp_dir}/exp/${simu_allspk_chunk2000_model_dir}/log
+    INIT_FILE=${exp_dir}/exp/${simu_allspk_chunk2000_model_dir}/ddp_init
+    if [ -f $INIT_FILE ];then
+        rm -f $INIT_FILE
+    fi
+    init_method=file://$(readlink -f $INIT_FILE)
+    echo "$0: init method is $init_method"
+    max_epoch=1
+    batch_size=8
+    lr=0.00001
+    for ((i = 0; i < $gpu_num; ++i)); do
+        {
+            rank=$i
+            local_rank=$i
+            gpu_id=$(echo $CUDA_VISIBLE_DEVICES | cut -d',' -f$[$i+1])
+            diar_train_eend_ola.py \
+                --gpu_id $gpu_id \
+                --use_preprocessor false \
+                --dataset_type $dataset_type \
+                --train_data_file $feats_dir/$dumpdir_chunk2000/${train_set}/${simu_scp} \
+                --valid_data_file $feats_dir/$dumpdir_chunk2000/${valid_set}/${simu_scp} \
+                --resume true \
+                --init_param ${exp_dir}/exp/${simu_allspk_model_dir}/${average_start}-${average_end}epoch.ave.pb \
+                --max-epochs ${max_epoch} \
+                --batchsize ${batch_size} \
+                --lr ${lr} \
+                --output_dir ${exp_dir}/exp/${simu_allspk_chunk2000_model_dir} \
+                --config $train_simu_allspk_config \
+                --input_size $feats_dim \
+                --ngpu $gpu_num \
+                --num_worker_count $count \
+                --unused_parameters true \
+                --multiprocessing_distributed true \
+                --dist_init_method $init_method \
+                --dist_world_size $world_size \
+                --dist_rank $rank \
+                --local_rank $local_rank 1> ${exp_dir}/exp/${simu_allspk_chunk2000_model_dir}/log/train.log.$i 2>&1
+        } &
+        done
+        wait
 fi
 
 ## Testing Stage
